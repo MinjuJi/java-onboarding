405e7a5f2592bcc9e7d5ec66cfae1ab79a92abfe
민주 <hinote444@gmail.com>
2023-08-31 15:51:22 +0900

    feat: Problem1 - 예외처리를 위한 checkException 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,18 +3,23 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-	
+
 	static final int LEFT_PAGE = 0;
 	static final int RIGHT_PAGE = 1;
 	static final int POBI_WIN = 1;
 	static final int CRONG_WIN = 2;
 	static final int DRAW = 0;
 	static final int EXCEPTION = -1;
-	
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-    }
-    
-    
+
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+		int answer = Integer.MAX_VALUE;
+		return answer;
+	}
+
+	public static boolean checkException(List<Integer> pages) {
+		if (pages.get(LEFT_PAGE) + 1 != pages.get(RIGHT_PAGE) || pages.get(LEFT_PAGE) % 2 == 0)
+			return true;
+		return false;
+	}
+
 }
