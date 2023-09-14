e6a7b09fdda5bdb9b8f5b03c8a92554c9d7055c0
민주 <hinote444@gmail.com>
2023-09-15 01:07:20 +0900

    feat: Problem2 - 문자열의 중복 문자가 있는지 반환해주는 isNotMoreDelete 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -11,5 +11,15 @@ public class Problem2 {
     boolean isLengthZero = cryptogram.length() == 0 ? true : false;
     return isLengthZero;
   }
+  
+  public static boolean isNotMoreDelete(String cryptogram) {
+    boolean isNotMoreDelete = true;
+    for (int i = 1; i < cryptogram.length(); i++) {
+      if (cryptogram.charAt(i) == cryptogram.charAt(i - 1)) {
+        return false;
+      }
+    }
+    return isNotMoreDelete;
+  }
 
 }
