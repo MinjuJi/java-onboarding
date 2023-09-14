989eb7c02f22ab579877d9cc877b8b1bc2b930f8
민주 <hinote444@gmail.com>
2023-09-15 01:05:02 +0900

    feat: Problem2 - 문자열의 길이가 0인지 확인해주는 isLengthZero 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,15 @@
 package onboarding;
 
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
-    }
+
+  public static String solution(String cryptogram) {
+    String answer = "answer";
+    return answer;
+  }
+
+  public static boolean isLengthZero(String cryptogram) {
+    boolean isLengthZero = cryptogram.length() == 0 ? true : false;
+    return isLengthZero;
+  }
+
 }
