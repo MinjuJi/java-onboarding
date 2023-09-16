33c245887eb7b3666cf8460f97026ebff60fa783
민주 <hinote444@gmail.com>
2023-09-17 04:45:30 +0900

    fear: Problem4 - 알파벳을 청개구리 사전에 해당하는 알파벳으로 바꿔주는 getFrogWord 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,20 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
+  public static String solution(String word) {
+    String answer = "";
+    return answer;
+  }
+
+  public static char getFrogWord(char alphabet) {
+    if (!Character.isAlphabetic(alphabet))
+      return alphabet;
+    if (Character.isUpperCase(alphabet)) {
+      return (char) ('A' + ('Z' - alphabet));
+    } else {
+      return (char) ('a' + ('z' - alphabet));
     }
+  }
+  
+  
 }
