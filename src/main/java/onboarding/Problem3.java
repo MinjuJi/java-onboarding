5a4644241000b575f3fcf06a02ccbd51165817a8
민주 <hinote444@gmail.com>
2023-09-16 12:53:29 +0900

    feat: Problem3 - 올바른 숫자의 범위인지 판단해주는 isValidRangeNumberException() 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,19 +5,29 @@ public class Problem3 {
   private static final char THREE = '3';
   private static final char SIX = '6';
   private static final char NINE = '9';
+  private static final int MIN_NUMBER = 1;
+  private static final int MAX_NUMBER = 10000;
 
   public static int solution(int number) {
     int answer = getAnswer(number);
     return answer;
   }
 
+
   public static int getAnswer(int number) {
+    if (!isValidRangeNumberException(number))
+      throw new IllegalArgumentException("1 ~ 10,000 자연수 입력해주세요.");
+
     int count = 0;
     for (int i = 1; i <= number; i++) {
       String numberStr = makeStringNumber(i);
       count += getClapCount(numberStr);
     }
     return count;
+  }
+
+  public static boolean isValidRangeNumberException(int number) {
+    return number >= MIN_NUMBER && number <= MAX_NUMBER;
   }
 
   public static String makeStringNumber(int number) {
