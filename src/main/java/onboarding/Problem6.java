7220b5007911d6383ca5795ed5ffac901b3bf0bf
민주 <hinote444@gmail.com>
2023-09-20 01:57:28 +0900

    feat: Problem6 - 상수 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,8 +3,21 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
-    public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
-    }
+
+  private static final int MIN_NUMBER_CREW = 1;
+  private static final int MAX_NUMBER_CREW = 10000;
+  private static final int MIN_LENGTH_EMAIL = 11;
+  private static final int MAX_LENGTH_EMAIL = 19;
+  private static final int MIN_LENGTH_NICKNAME = 1;
+  private static final int MAX_LENGTH_NICKNAME = 19;
+  private static final int INDEX_EMAIL = 0;
+  private static final int INDEX_NICKNAME = 1;
+  private static final String REGEX_EMAIL = "\\w+@\\w+\\.\\w+(\\.\\w+)?";
+  private static final String DOMAIN_EMAIL = "email.com";
+  private static final String REGEX_NICKNAME = "^[가-힣]*$";
+
+  public static List<String> solution(List<List<String>> forms) {
+    List<String> answer = List.of("answer");
+    return answer;
+  }
 }
