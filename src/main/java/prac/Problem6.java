c7c5f5bb1dd96595dfd752624264abb6b131c011
민주 <hinote444@gmail.com>
2023-09-20 03:08:37 +0900

    style: Problem6 - 변수명 수정

--- src/main/java/prac/Problem6.java
+++ src/main/java/prac/Problem6.java
@@ -8,17 +8,17 @@ import java.util.stream.Collectors;
 
 public class Problem6 {
 
-  private static final int MIN_CREW_COUNT = 1;
-  private static final int MAX_CREW_COUNT = 10000;
-  private static final int MIN_EMAIL_LENGTH = 11;
-  private static final int MAX_EMAIL_LENGTH = 20;
-  private static final int MIN_NICKNAME_LENGTH = 1;
-  private static final int MAX_NICKNAME_LENGTH = 20;
-  private static final String NAME_REGEX = "^[가-힣]*$";
-  private static final String EMAIL_REGEX = "\\w+@\\w+\\.\\w+(\\.\\w+)?";
-  private static final String VALID_DOMAIN = "email.com";
-  private static final int EMAIL_INDEX = 0;
-  private static final int NICKNAME_INDEX = 1;
+  private static final int MIN_NUMBER_CREW = 1;
+  private static final int MAX_NUMBER_CREW = 10000;
+  private static final int MIN_LENGTH_EMAIL = 11;
+  private static final int MAX_LENGTH_EMAIL = 19;
+  private static final int MIN_LENGTH_NICKNAME = 1;
+  private static final int MAX_LENGTH_NICKNAME = 19;
+  private static final int INDEX_EMAIL = 0;
+  private static final int INDEX_NICKNAME = 1;
+  private static final String REGEX_EMAIL = "\\w+@\\w+\\.\\w+(\\.\\w+)?";
+  private static final String DOMAIN_EMAIL = "email.com";
+  private static final String REGEX_NICKNAME = "^[가-힣]*$";
 
   public static List<String> solution(List<List<String>> forms) {
     checkException(forms);
@@ -26,19 +26,18 @@ public class Problem6 {
   }
 
   private static List<String> getAnswer(List<List<String>> forms) {
-    HashMap<String, String> separatedNicknameToEmail = new HashMap<>();
+    HashMap<String, String> separatedNicknameAndEmail = new HashMap<>();
 
     List<String> answer = forms.stream()
-                               .map(form -> getInvalidEmails(separatedNicknameToEmail, form))
+                               .map(form -> getInvalidEmails(separatedNicknameAndEmail, form))
                                .flatMap(Collection::stream)
                                .distinct()
                                .sorted()
                                .collect(Collectors.toList());
-
     return answer;
   }
 
-  private static List<String> getInvalidEmails(HashMap<String, String> separatedNicknameToEmail, List<String> form) {
+  private static List<String> getInvalidEmails(HashMap<String, String> separatedNicknameAndEmail, List<String> form) {
     List<String> result = new ArrayList<>();
 
     String email = getEmail(form);
@@ -46,9 +45,9 @@ public class Problem6 {
 
     for (int i = 0; i < nickname.length() - 1; i++) {
       String separatedNickname = nickname.substring(i, i + 2);
-      String invalidEmail = separatedNicknameToEmail.putIfAbsent(separatedNickname, email);
-      if (invalidEmail != null) {
-        result.add(invalidEmail);
+      String InvalidEmail = separatedNicknameAndEmail.putIfAbsent(separatedNickname, email);
+      if (InvalidEmail != null) {
+        result.add(InvalidEmail);
         result.add(email);
       }
     }
@@ -56,59 +55,59 @@ public class Problem6 {
   }
 
   private static String getEmail(List<String> form) {
-    return form.get(EMAIL_INDEX);
+    return form.get(INDEX_EMAIL);
   }
 
   private static String getNickname(List<String> form) {
-    return form.get(NICKNAME_INDEX);
+    return form.get(INDEX_NICKNAME);
   }
 
   private static void checkException(List<List<String>> forms) {
     if (!isValidCrewRange(forms))
-      throw new IllegalArgumentException("크루원은 1명 이상 10,000명 이하입니다.");
+      throw new IllegalArgumentException("크루원은 1명 이상 10,000 이하입니다.");
 
     if (!isValidEmailRegex(forms))
       throw new IllegalArgumentException("이메일 형식(@)으로 입력해주세요.");
 
     if (!isValidDomain(forms))
-      throw new IllegalArgumentException("유효한 이메일의 도메인은 [email.com] 입니다.");
+      throw new IllegalArgumentException("유효한 이메일 도메인은 [email.com] 입니다.");
 
     if (!isValidEmailLength(forms))
-      throw new IllegalArgumentException("유효한 이메일의 길이는 11 ~ 19자 입니다.");
+      throw new IllegalArgumentException("유효한 이메일의 길이는 11자 이상 20자 미만입니다.");
 
     if (!isValidNicknameRegex(forms))
       throw new IllegalArgumentException("닉네임은 한글만 가능합니다.");
 
     if (!isValidNicknameLength(forms))
-      throw new IllegalArgumentException("유효한 닉네임의 길이는 1 ~ 19자 입니다.");
+      throw new IllegalArgumentException("유효한 닉네임의 길이는 1자 이상 20자 미만입니다.");
   }
 
   private static boolean isValidCrewRange(List<List<String>> forms) {
-    return forms.size() >= MIN_CREW_COUNT && forms.size() <= MAX_CREW_COUNT;
+    return forms.size() >= MIN_NUMBER_CREW && forms.size() <= MAX_NUMBER_CREW;
   }
 
   private static boolean isValidEmailRegex(List<List<String>> forms) {
     return forms.stream()
-                .allMatch(form -> getEmail(form).matches(EMAIL_REGEX));
+                .allMatch(form -> getEmail(form).matches(REGEX_EMAIL));
   }
 
   private static boolean isValidDomain(List<List<String>> forms) {
     return forms.stream()
-                .allMatch(form -> getEmail(form).contains(VALID_DOMAIN));
+                .allMatch(form -> getEmail(form).contains(DOMAIN_EMAIL));
   }
 
   private static boolean isValidEmailLength(List<List<String>> forms) {
     return forms.stream()
-                .allMatch(form -> getEmail(form).length() >= MIN_EMAIL_LENGTH && getEmail(form).length() < MAX_EMAIL_LENGTH);
+                .allMatch(form -> getEmail(form).length() >= MIN_LENGTH_EMAIL && getEmail(form).length() <= MAX_LENGTH_EMAIL);
   }
 
   private static boolean isValidNicknameRegex(List<List<String>> forms) {
     return forms.stream()
-                .allMatch(form -> getNickname(form).matches(NAME_REGEX));
+                .allMatch(form -> getNickname(form).matches(REGEX_NICKNAME));
   }
 
   private static boolean isValidNicknameLength(List<List<String>> forms) {
     return forms.stream()
-                .allMatch(form -> getNickname(form).length() >= MIN_NICKNAME_LENGTH && getNickname(form).length() < MAX_NICKNAME_LENGTH);
+                .allMatch(form -> getNickname(form).length() >= MIN_LENGTH_NICKNAME && getNickname(form).length() <= MAX_LENGTH_NICKNAME);
   }
 }
