9ffbae14debb2c7e84ea1d5fa30d23bc937e6623
민주 <hinote444@gmail.com>
2023-09-22 10:18:07 +0900

    Problem7 테스트 케이스 추가

--- src/main/java/prac/Test7.java
+++ src/main/java/prac/Test7.java
@@ -6,6 +6,7 @@ public class Test7 {
   public static void main(String[] args) {
 
     String user = "mrko";
+
     List<List<String>> friends = List.of(
         List.of("donut", "andole"),
         List.of("donut", "jun"),
@@ -14,11 +15,17 @@ public class Test7 {
         List.of("shakevan", "jun"),
         List.of("shakevan", "mrko"));
     List<String> visitors = List.of("bedi", "bedi", "donut", "bedi", "shakevan");
-
     List<String> answer = Problem7.solution(user, friends, visitors);
     System.out.println(answer);
-
     // List<String> result = List.of("andole", "jun", "bedi");
+
+    //    List<List<String>> friends2 = List.of(
+    //        List.of("donut", "mrko"),
+    //        List.of("shakevan", "mrko"));
+    //    List<String> visitors2 = List.of("donut", "minju", "shakevan");
+    //    List<String> answer2 = Problem7.solution(user, friends2, visitors2);
+    //    System.out.println(answer2);
+    //    // List<String> result = List.of("minju");
 
   }
 }
