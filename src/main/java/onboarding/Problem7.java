8237010a87f63da4596db0a14e1ea3ea78ec60ad
민주 <hinote444@gmail.com>
2023-09-22 14:32:17 +0900

    feat: Problem7 - 상수 및 변수 추가

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,27 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
-    }
+  private static final int FRIENDS_POINT = 10;
+  private static final int VISIORS_POINT = 1;
+  private static final int MIN_ID_LENGTH = 1;
+  private static final int MAX_ID_LENGTH = 30;
+  private static final int MIN_FRIENDS_SIZE = 1;
+  private static final int MAX_FRIENDS_SIZE = 10000;
+  private static final int MIN_VISITORS_SIZE = 0;
+  private static final int MAX_VISITORS_SIZE = 10000;
+  private static final String ID_REGEX_LOWERCASE = "^[a-z]*$";
+
+  private static Set<String> userFriendsSet = new HashSet<>();
+  private static Map<String, Integer> userAndScoreMap = new HashMap<>();
+
+  public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+    return null;
+  }
+
 }
